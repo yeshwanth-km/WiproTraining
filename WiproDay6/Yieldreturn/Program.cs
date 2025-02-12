b79b25eccb6fcd25f30fad36a668a75c6f9ceea6
yeshwanth-km <yeshwanthkm32@gmail.com>
2025-02-12 15:09:33 +0530

    WiproDay6

---
+++ WiproDay6/Yieldreturn/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach (string country in PrintCountry('u'))
+        {
+            Console.WriteLine(country);
+        }
+    }
+    static IEnumerable<string> PrintCountry(char chr) //u
+    {
+        List<string> countries = new List<string>() { "India", "US", "UK", "Europe" }; //original list
+
+        //  List<string> countrieswithchar = new List<string>(); //empty list
+
+        for (int i = 0; i < countries.Count; i++)
+        {
+            if (countries[i].ToLower().Contains(chr))//charcter - u
+            {
+                Console.WriteLine(i.ToString());
+                //  countrieswithchar.Add(countries[i]);
+                yield return countries[i]; //iteration blocker  - provide the next value in the iteration
+            }
+        }
+        //return countrieswithchar;
+    }
+}
