a4a6ecfddae3b30b1d0112ac2a3e73f72c3e0718
yeshwanth-km <yeshwanthkm32@gmail.com>
2025-02-19 16:31:10 +0530

    WiproDay10

---
+++ Realibility/Models/Employee.cs
@@ -0,0 +1,8 @@
+namespace WiproDemoRazorApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+    }
+}
---
+++ Realibility/Pages/Sample.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WiproDemoRazorApp.Pages
+{
+    public class SampleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
